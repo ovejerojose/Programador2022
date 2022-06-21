42cdf2351dfafddf490b6fa03e7f4bfb82c8b454
Jose Alberto Ovejero <106357119+ovejerojose@users.noreply.github.com>
2022-06-21 19:22:24 -0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -14,3 +14,24 @@ while c0 != 1:
 
 print("los pasos: ", pasos)
 
+Agrego la solucion del punto 3.2.1.14 de la pirámide:
+    
+blocks = int(input("Ingresa el número de bloques: "))
+height = 0
+i = 1
+bloques_necesarios_para_este_piso = 1
+bloques_usados = 0
+
+while i <= blocks:
+    #print("imprimo i : ",i)
+    
+    if i - bloques_usados == bloques_necesarios_para_este_piso:
+        #print("bloques necesarios: ",bloques_necesarios_para_este_piso)
+        bloques_usados += bloques_necesarios_para_este_piso
+        bloques_necesarios_para_este_piso += 1
+        height += 1
+     #   print("para el proximo nivel necesito : ",bloques_necesarios_para_este_piso, "bloques")
+     #   print("imprimo altura :",height)
+    i += 1
+
+print("La altura de la pirámide:", height)

de95469ccffbccd62879e84ea205c0e19ecd83d2
Joaquín Suarez <106562080+JoaquinSuarezV@users.noreply.github.com>
2022-08-12 21:37:47 -0300

    Create Historial5.py

---
+++ Historial5.py
@@ -0,0 +1,5 @@
+#Esta tupla representara el monto total en pesos, por los diferentes servicios/consultas que recibio la mascota “Toto”.
+
+tupla = (8520, 9510, 7530, 3570, 1590, "Atencion gastada en Toto: ")#Ingresamos valores en la tupla, en este caso los diferentes montos.
+
+print(tupla[5], tupla[0] + tupla[1] + tupla[2] + tupla[3] + tupla[4])#Sumamos todos los valores, obteniendo finalmente el monto total.
